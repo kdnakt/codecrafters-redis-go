b6ccc19330ad6ad0b5f1191ada111150aacc372d
kdnakt <a.kid.1985@gmail.com>
2023-01-26 09:34:29 +0900

    Respond to multiple PINGs

--- app/server.go
+++ app/server.go
@@ -17,11 +17,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handle(conn)
 	}
+}
+
+func handle(conn net.Conn) {
 	for {
 		message := make([]byte, 128)
 		n, err := conn.Read(message)
